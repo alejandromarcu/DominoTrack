0d2807472a3be90909bfd21c325b199d3519ee78
Alejandro Marcu <amarcu@gmail.com>
2017-12-15 22:57:04 -0800

    Testing.cs => PlaceCursorObjectController

---
+++ DominoTrack/Assets/Scripts/PlaceCursorObjectController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Switch between edit and place modes depending on wheteher there's a collision between the cursor and other pieces
+ */
+public class PlaceCursorObjectController : MonoBehaviour {
+
+	private GameObject currentCollision = null;
+    public GameObject cursor;
+
+    void OnTriggerEnter(Collider other) {
+		if (currentCollision != null)
+			return;
+		
+		if (other.CompareTag("domino")) {
+            cursor.GetComponent<CursorController>().EditMode(other.transform.parent.gameObject);
+			currentCollision = other.gameObject;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject == currentCollision) {
+            cursor.GetComponent<CursorController>().PlaceMode ();
+			currentCollision = null;
+		}
+	}
+}

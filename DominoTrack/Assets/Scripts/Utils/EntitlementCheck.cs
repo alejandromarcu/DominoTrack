64dc345f6ada7732894bcc631316d0401a71f34f
Alejandro Marcu <amarcu@gmail.com>
2018-01-21 00:05:22 -0800

    Entitlement check for Oculus

---
+++ DominoTrack/Assets/Scripts/Utils/EntitlementCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Platform;
+
+public class EntitlementCheck : MonoBehaviour
+{
+
+    void Start()
+    {
+        Oculus.Platform.Core.Initialize("1641825905840347");
+        Oculus.Platform.Entitlements.IsUserEntitledToApplication().OnComplete(EntitlementChecked);
+    }
+
+    void EntitlementChecked(Message msg)
+    {
+        if (msg.IsError)
+        {
+            Debug.LogError("Entitlement failed: " + msg);
+            UnityEngine.Application.Quit();
+        }
+    }
+}
